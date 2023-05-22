47f1775402ac909bb6518af6e17ae6a54ad14bcc
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-05-22 07:03:09 +0700

    restAPI laravel 10

--- database/migrations/2023_05_21_214450_create_posts_table.php
+++ database/migrations/2023_05_21_214450_create_posts_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('image');
+            $table->string('title');
+            $table->string('content');
             $table->timestamps();
         });
     }
